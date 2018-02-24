cdd61fd133d4c04fc9534fd02b5920c9b721efaf
bycuimiao <373013128@qq.com>
2018-02-24 17:14:30 +0800

    api commit

---
+++ api/src/main/java/com/xinao/hello/Hello.java
@@ -0,0 +1,33 @@
+package com.xinao.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @auther cuimiao
+ * @date 2018/2/24/024  12:02
+ * @Description: controller demo.
+ */
+@Controller
+public class Hello {
+
+    @Autowired
+    private HelloService helloService;
+
+    public Hello(){
+        System.out.println("init hello");
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public void hello(){
+        List<String> list = Arrays.asList("Hello", "World!");
+        list.stream().forEach(System.out::println);
+    }
+}
---
+++ api/src/main/java/com/xinao/hello/HelloService.java
@@ -0,0 +1,15 @@
+package com.xinao.hello;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @auther cuimiao
+ * @date 2018/2/24/024  15:24
+ * @Description: ${todo}
+ */
+@Service
+public class HelloService {
+    public HelloService(){
+        System.out.println("init HelloService");
+    }
+}
