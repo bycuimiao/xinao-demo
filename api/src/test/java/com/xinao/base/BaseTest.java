1dded111f5f7275182bebfc9625b7f3ae8af1301
bycuimiao <373013128@qq.com>
2018-03-29 00:23:50 +0800

    实现maven动态替换配置文件

--- api/src/test/java/com/xinao/base/BaseTest.java
+++ api/src/test/java/com/xinao/base/BaseTest.java
@@ -1,5 +1,6 @@
 package com.xinao.base;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,4 +14,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class) //使用junit4进行测试
 @ContextConfiguration(locations={"classpath:applicationContext.xml"}) //加载配置文件
 public class BaseTest {
+
+  @Test
+  public void bastTest(){
+    System.out.println("hello BaseTest");
+  }
 }
---
+++ api/src/test/java/com/xinao/base/TempTest.java
@@ -0,0 +1,21 @@
+/*
+ * Created by cuimiao on 2018/3/28.
+ */
+
+package com.xinao.base;
+
+import org.junit.Test;
+
+/**
+ * @author cuimiao
+ * @version 0.0.1
+ * @Description:
+ * @since 0.0.1 2018-03-28
+ */
+public class TempTest extends BaseTest {
+
+  @Test
+  public void tempTest(){
+    System.out.println("hello test");
+  }
+}
